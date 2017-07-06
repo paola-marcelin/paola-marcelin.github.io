422d55a6ee75cf32f82311510052a7daa405a546
data4 <alejandro.carreno@data4.mx>
2017-07-06 15:11:59 -0500

    add: footer, portfolio y link a repositorio git hub

---
+++ script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+});
